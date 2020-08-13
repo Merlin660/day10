819a78dde620863a778ef99d48ece185ecae318d
Merlin660 <2937407342@qq.com>
2020-08-13 09:59:46 +0800

    map1.0

---
+++ day10/src/day10/MapDemo.java
@@ -0,0 +1,90 @@
+package day10;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class MapDemo {
+	
+	
+	@Test
+	public void demo1() {
+		
+		// Map的获取
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		
+	}
+	
+	@Test
+	public void demo2() {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		
+		// Map的添加
+		map.put("110", "张三1");
+		map.put("120", "张三2");
+		map.put("130", "张三3");
+		map.put("140", "张三4");
+		
+		System.out.println(map);
+	}
+	
+	
+	@Test
+	public void demo3() {
+		Map<String, String> map = createMap();
+		System.out.println(map);
+		System.out.println("===========");
+		
+		// 删除
+		map.remove("4");
+		System.out.println(map);
+		System.out.println("============");
+		
+		map.remove("3");
+		System.out.println(map);
+		
+		
+	}
+	
+	
+	public Map<String, String> createMap() {
+		Map<String, String> map = new HashMap<String, String>();
+		for (int i = 0; i < 6; i++) {
+			map.put("" + i, "张三" + i);
+		}
+		
+		return map;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
